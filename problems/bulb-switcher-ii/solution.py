a90fc9327e4a74c5cfa04e242203b13461897fdf
bhaskarreddy02 <bhaskar.pathakuntla@gmail.com>
2026-07-31 21:58:21 +0530

    feat: add LeetCode 672. Bulb Switcher II solution (python3)

---
+++ problems/bulb-switcher-ii/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def flipLights(self, n: int, presses: int) -> int:
+        n = min(n, 3)
+
+        if presses == 0:
+            return 1
+
+        if n == 1:
+            return 2
+
+        if n == 2:
+            if presses == 1:
+                return 3
+            return 4
+
+        # n == 3
+        if presses == 1:
+            return 4
+        if presses == 2:
+            return 7
+        return 8
