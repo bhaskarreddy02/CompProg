8809c4a5e849fcc76741c52eac4c1f3d21b748a9
bhaskarreddy02 <bhaskar.pathakuntla@gmail.com>
2026-08-05 09:55:15 +0530

    feat: add LeetCode 312. Burst Balloons solution (cpp)

---
+++ problems/burst-balloons/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i > j) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int ans = 0;
+
+        for (int k = i; k <= j; k++) {
+
+            int cost = solve(i, k - 1, nums, dp) + solve(k + 1, j, nums, dp) +
+                       nums[i - 1] * nums[k] * nums[j + 1];
+
+            ans = max(ans, cost);
+        }
+        return dp[i][j]=ans;
+    }
+
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return solve(1, n - 2, nums, dp);
+    }
+};
